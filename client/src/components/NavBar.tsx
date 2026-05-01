47deb6e02c8169ed4982ad0e1837a76630d35375
Verde <sofia.16d@gmail.com>
2026-05-01 17:34:23 -0300

    boton menu color

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -31,19 +31,22 @@ const NavBar = (): ReactElement => {
       }}
     >
       <Container fluid className="d-flex flex-column justify-content-between h-100 py-2" style={{ minHeight: '150px' }}>
-        
+
         {/* SECCIÓN SUPERIOR */}
         <div className="d-flex justify-content-between align-items-center w-100">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0 bg-transparent p-0" style={{ zIndex: 1020 }}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" style={{ zIndex: 1020 }}>
             <div className="d-flex flex-column align-items-center">
               <img src={MenuIcon} alt="Menú" width={50} height={50} />
-              <small className="fw-bold" style={{ fontSize: '18px' , color: 'var(--blanco' }}>MENÚ</small>
+              <small className="fw-bold" style={{ fontSize: '18px', color: 'var(--gris)' }}>MENÚ</small>
             </div>
           </Navbar.Toggle>
 
-          <Nav.Link as={Link} to="/perfil" className="d-flex flex-column align-items-center">
-            <img src={PerfilIcon} alt="Perfil" width={50} height={50} />
-            <small style={{ color: 'var(--blanco', fontSize: '18px', fontWeight: 'bold' }}>PERFIL</small>
+          <Nav.Link as={Link} to="/perfil" className="d-flex flex-column align-items-center" style={{ zIndex: 1020 }}>
+            <div className="d-flex flex-column align-items-center">
+
+              <img src={PerfilIcon} alt="Perfil" width={50} height={50} />
+              <small style={{ color: 'var(--blanco', fontSize: '18px', fontWeight: 'bold' }}>PERFIL</small>
+            </div>
           </Nav.Link>
         </div>
 
@@ -61,7 +64,7 @@ const NavBar = (): ReactElement => {
             <Nav.Link as={Link} to="/trabajos">TRABAJOS</Nav.Link>
             <Nav.Link as={Link} to="/viajes">VIAJES</Nav.Link>
             <Nav.Link as={Link} to="/animales">MASCOTAS</Nav.Link>
-            
+
             <hr className="bg-dark" />
             <Nav.Link as={Link} to="/perfil" className="d-flex flex-column align-items-center pt-2">
               <img src={PerfilIcon} alt="Perfil" width={40} height={40} />
