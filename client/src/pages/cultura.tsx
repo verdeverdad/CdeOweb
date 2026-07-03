d92d89a529243ed1a5fcae1cd81693f18b68a875
Verde <sofia.16d@gmail.com>
2026-07-03 20:47:11 -0300

    recuperacion

--- client/src/pages/cultura.tsx
+++ client/src/pages/cultura.tsx
@@ -4,7 +4,7 @@ import { CATEGORIAS } from "../types";
 export default function Cultura() {
     return (
         <>
-        <h1>dalee</h1>
+        <h1 style={{ color: "red"} }>C U L T U R A</h1>
             <MostrarPublicaciones filtroCategoria={CATEGORIAS.CULTURA} />
 
         </>
